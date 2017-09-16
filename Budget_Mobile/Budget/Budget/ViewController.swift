0b3720079fd8c3efab6392d13540a9b8d40fdd10
Thomas Youngson <thomas_youngson@hotmail.com>
2017-09-16 22:47:10 +1200

    added app for IOS

--- Budget_Mobile/Budget/Budget/ViewController.swift
+++ Budget_Mobile/Budget/Budget/ViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
+    
+    @IBOutlet weak var incomeLabel: UILabel!
+    @IBOutlet weak var incomeTextField: UITextField!
+    
+    @IBOutlet weak var rentLabel: UILabel!
+    @IBOutlet weak var rentTextField: UITextField!
+    
+    @IBOutlet weak var foodLabel: UILabel!
+    @IBOutlet weak var foodTextField: UITextField!
+    
+    @IBOutlet weak var fuelLabel: UILabel!
+    @IBOutlet weak var fuelTextField: UITextField!
+    
+    @IBOutlet weak var totalLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the income text field user input through delegate callbacks.
+        incomeTextField.delegate = self
+        incomeTextField.keyboardType = .numberPad
+        
+        rentTextField.delegate = self
+        rentTextField.keyboardType = .numberPad
+        
+        foodTextField.delegate = self
+        foodTextField.keyboardType = .numberPad
+        
+        fuelTextField.delegate = self
+        fuelTextField.keyboardType = .numberPad
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func calculateTotal(_ sender: UIButton) {
+        
+        var calTotal = Double()
+        
+        // Only allows numbers to be passed in with defualt of 0.
+        let incomeDouble = Double(incomeTextField.text!) ?? 0
+        let rentDouble = Double(rentTextField.text!) ?? 0
+        let foodDouble = Double(foodTextField.text!) ?? 0
+        let fuelDouble = Double(fuelTextField.text!) ?? 0
+        
+        // Calculates the left over budget.
+        calTotal = incomeDouble - (rentDouble + foodDouble + fuelDouble)
+        
+        totalLabel.text = "$" + String(calTotal)
+    }
 }
 
